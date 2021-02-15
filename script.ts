d640e967e770ce673b89694aa403fa4f5815cd01
Danil <dinkodanil@gmail.com>
2021-02-15 19:29:43 +0300

    Learn Types

---
+++ script.ts
@@ -0,0 +1,15 @@
+type Kind = {
+    name: string,
+    title: string,
+    desc: string,
+    age: number,
+    isGay: boolean,
+}
+
+let Stefan: Kind = {
+    name: "Stefan",
+    title: "Stefan Gaming",
+    desc: "I am not gay",
+    age: 15,
+    isGay: false
+}
